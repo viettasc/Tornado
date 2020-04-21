90ba13ec14ef1425b8db1375d13ef9e378acd625
viettasc <51367503+viettasc@users.noreply.github.com>
2020-04-21 09:41:47 +0700

    Initial Commit

---
+++ Day92/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day92
+//
+//  Created by Viettasc Doan on 4/21/20.
+//  Copyright © 2020 Viettasc Doan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
